dabf815a15efb5cfacb47ae63b7a592f6f82b4ff
goodcodedev <gudmund@goodcode.no>
2017-11-29 12:47:28 +0100

    Ignore target dirs

---
+++ target/rls/debug/deps/memchr-6f5ca9ed2df02dfd.d
@@ -0,0 +1,5 @@
+/Users/gudmund/rust/descr/target/rls/debug/deps/memchr-6f5ca9ed2df02dfd.rmeta: Users/gudmund/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-1.0.2/src/lib.rs
+
+/Users/gudmund/rust/descr/target/rls/debug/deps/memchr-6f5ca9ed2df02dfd.d: Users/gudmund/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-1.0.2/src/lib.rs
+
+Users/gudmund/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-1.0.2/src/lib.rs:
